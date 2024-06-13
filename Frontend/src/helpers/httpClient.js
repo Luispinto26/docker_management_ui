b4b384006ed0c3ef96ca922c6c208a6f4f98bd41
Luis Pinto <luis.pinto@ludustechnology.com>
2024-06-13 13:09:49 +0100

    Change ip hardcoded

--- Frontend/src/helpers/httpClient.js
+++ Frontend/src/helpers/httpClient.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const apiUrl = 'http://localhost:3000/api/database/'
+const apiUrl = "http://192.168.1.84:3000/api/database/";
 
 const httpRequest = axios.create({
-    baseURL: apiUrl,
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-export {httpRequest}
+export { httpRequest };
